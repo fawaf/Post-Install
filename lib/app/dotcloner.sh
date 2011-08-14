28db1529bb348ddf151f44165e6041f341581a5d
David Mah <mahhaha@gmail.com>
2011-08-13 20:30:02 -0700

    Added my gconf directory
    - You don't want this because it is ridiculously everything configured on
        my laptop. Even the hardcoded pathnames of my wallpapers. but i put
        it in anyway

--- lib/app/dotcloner.sh
+++ lib/app/dotcloner.sh
@@ -5,16 +5,17 @@ source $1/lib/app/helper.sh $1
 # $2 is the dotfile to substitute in
 function replace_dotfile {
   echo "This will replace your current $1,"
-  echo -n "enter y if this is alright: "
+  echo -n "enter [y] if this is alright: "
   read -p "" REPLY
   if [[ ! (("$REPLY" == "y") || ("$REPLY" == "Y")) ]]; then
     echo "You said no. So This will not continue"
     return
   fi
   echo "Backing up $1 to $1.backup"
-  $(cp ~/.$1 ~/.$1.backup)
+  $(cp -R ~/.$1 ~/.$1.backup)
   echo "Replacing $1 with $2"
-  $(cp $2 ~/.$1)
+  $(rm -rf ~/.$1)
+  $(cp -R $2 ~/.$1)
 }
 function choose_dotfile {
   echo ""
