155baa8efc47936922ae683c883e35ada8fde637
DungFu <dungfu3@gmail.com>
2011-08-16 13:10:10 -0700

    Improved the Google Chrome installer and got rid of the logging stuff in install_node.sh

--- lib/installs/install_google_chrome.sh
+++ lib/installs/install_google_chrome.sh
@@ -2,8 +2,29 @@
 
 determine_bitage
 VER=$RETURN
+
+VC=""
+
+while true; do
+  read -p "Choose which Google Chrome version you wish to install (stable = 1, beta = 2, unstable = 3): " VCHROME
+  if [ "$VCHROME" = 1 ]; then
+    VC="stable"
+    break
+  elif [ "$VCHROME" = 2 ]; then
+    VC="beta"
+    break
+  elif [ "$VCHROME" = 3 ]; then
+    VC="unstable"
+    break
+  else
+    echo ""
+    echo "Invalid Choice!"
+    echo ""
+  fi
+done
+
 echo "Installing Google Chrome..."
 sudo apt-get -qqy install lsb-core
-wget http://dl.google.com/linux/direct/google-chrome-stable_current_${VER}.deb -O ~/Downloads/google-chrome-stable_current_${VER}.deb
-sudo dpkg -i ~/Downloads/google-chrome-stable_current_${VER}.deb
-rm ~/Downloads/google-chrome-stable_current_${VER}.deb
+wget http://dl.google.com/linux/direct/google-chrome-${VC}_current_${VER}.deb -O ~/Downloads/google-chrome-${VC}_current_${VER}.deb
+sudo dpkg -i ~/Downloads/google-chrome-${VC}_current_${VER}.deb
+rm ~/Downloads/google-chrome-${VC}_current_${VER}.deb
