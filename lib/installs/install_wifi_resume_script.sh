6e637e83751f470a531a73162494ae5d9bffc795
DungFu <dungfu3@gmail.com>
2011-08-15 17:41:18 -0700

    one last fix to wifi restart app.

--- lib/installs/install_wifi_resume_script.sh
+++ lib/installs/install_wifi_resume_script.sh
@@ -8,15 +8,15 @@ if [ -e "/usr/local/bin/wifi-restart" ]; then
 	sudo rm /usr/local/bin/wifi-restart
 fi
 
-sudo touch /usr/local/bin/wifi-restart
-sudo chmod +x /usr/local/bin/wifi-restart
+touch /usr/local/bin/wifi-restart
+chmod +x /usr/local/bin/wifi-restart
 echo -e '#!/bin/bash\nkillall nm-applet\ngksu service network-manager restart\nnm-applet\nexit' | sudo tee /usr/local/bin/wifi-restart
 
 if [ -e "/usr/local/bin/wifi.png" ]; then
 	sudo rm /usr/local/bin/wifi.png
 fi
 
-sudo cp "${ROOT}/config/wifi-resume-script/wifi.png" /usr/local/bin/
+cp "${ROOT}/config/wifi-resume-script/wifi.png" /usr/local/bin/
 
 if [ -e "/usr/share/applications/wifirestart.desktop" ]; then
 	sudo rm /usr/share/applications/wifirestart.desktop
