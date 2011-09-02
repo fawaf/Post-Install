0ba61073576eb6036fd481f5b96085f34590ed13
DungFu <dungfu3@gmail.com>
2011-09-01 17:07:55 -0700

    fixed truecrypt install to handle new versions

--- lib/installs/install_truecrypt.sh
+++ lib/installs/install_truecrypt.sh
@@ -1,13 +1,23 @@
 # Downloads and Installs Truecrypt
 
-echo "Installing Truecrypt..."
-if (uname -a | grep 'x86_64'); then
-  VER="x64"
+wget http://www.truecrypt.org/downloads -O ~/Downloads/truecrypt-index.html
+VSTABLE=`egrep -o "Latest Stable Version - .*</h1>" ~/Downloads/truecrypt-index.html`
+TVER=`echo "${VSTABLE}"|cut -d " " -f 5|cut -d "<" -f 1`
+
+read -p "Install Truecrypt v${TVER} (y/n)? " CONFIRM
+echo ""
+if [ "$CONFIRM" = n ]; then
+  echo "Quiting..."
 else
-  VER="x86"
+	echo "Installing Truecrypt..."
+	if (uname -a | grep 'x86_64'); then
+	  VER="x64"
+	else
+	  VER="x86"
+	fi
+	wget -q http://www.truecrypt.org/download/truecrypt-${TVER}-linux-${VER}.tar.gz -O ~/Downloads/truecrypt-${TVER}-linux-${VER}.tar.gz
+	tar -zxvf ~/Downloads/truecrypt-${TVER}-linux-${VER}.tar.gz -C ~/Downloads/Truecrypt/
+	rm ~/Downloads/truecrypt-${TVER}-linux-${VER}.tar.gz
+	sudo ~/Downloads/Truecrypt/truecrypt-${TVER}-setup-${VER}
+	rm -rf ~/Downloads/Truecrypt/
 fi
-wget -q http://www.truecrypt.org/download/truecrypt-7.0a-linux-${VER}.tar.gz -O ~/Downloads/truecrypt-7.0a-linux-${VER}.tar.gz
-tar -zxvf ~/Downloads/truecrypt-7.0a-linux-${VER}.tar.gz -C ~/Downloads/Truecrypt/
-rm ~/Downloads/truecrypt-7.0a-linux-${VER}.tar.gz
-sudo ~/Downloads/Truecrypt/truecrypt-7.0a-setup-${VER}
-rm -rf ~/Downloads/Truecrypt/
