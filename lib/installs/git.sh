eab5c1cb8336e718140e506d3de0f528c4ef2f9b
David Mah <mahhaha@gmail.com>
2011-09-11 21:30:22 -0700

    git fix

--- lib/installs/git.sh
+++ lib/installs/git.sh
@@ -1,6 +1,16 @@
 sudo apt-get install git
-git config --global user.name "David Mah"
-git config --global user.email Mahhaha@gmail.com
+
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
+
+read -p "Github User Name: " USERNAME
+read -p "Github Email    : " EMAIL
+
+git config --global user.name "$USERNAME"
+git config --global user.email $EMAIL
+
 echo "Proceed to https://github.com/account/ssh and add your ssh key to your account."
 echo "This is your ssh key, and it is now in your clipboard\n"
 cat ~/.ssh/id_*.pub | xclip -selection clipboard
