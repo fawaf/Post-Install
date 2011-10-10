aeb00ddcbb35f59e4238d7cb5424f239851d37ae
David Mah <mahhaha@gmail.com>
2011-10-10 16:07:57 -0700

    Git is a dependency for janus!!!

--- lib/installs/vim.sh
+++ lib/installs/vim.sh
@@ -2,5 +2,5 @@
 # Graphical Vim and Janus plugins for the best editor in the world
 # https://github.com/carlhuda/janus
 
-sudo apt-get install curl rake ruby-dev exuberant-ctags vim-gnome
+sudo apt-get install git curl rake ruby-dev exuberant-ctags vim-gnome
 curl https://raw.github.com/carlhuda/janus/master/bootstrap.sh -o - | sh
