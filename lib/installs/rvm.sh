fbb30cdb92ccc1f0d06e9d48b9b2a081f92f1b30
David Mah <Mahhaha@gmail.com>
2011-10-18 18:44:43 -0700

    RVM install patch to load rvm as a function

--- lib/installs/rvm.sh
+++ lib/installs/rvm.sh
@@ -9,6 +9,7 @@
 sudo apt-get install curl git
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bashrc
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 source ~/.bashrc
 rvm install 1.9.2
 rvm use --default 1.9.2
