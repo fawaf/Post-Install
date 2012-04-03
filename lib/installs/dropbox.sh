7f4af1923c316f77ca9e17a489047f7cd68bd222
DavidMah <Mahhaha@gmail.com>
2012-04-03 12:47:58 -0700

    Use apt-get to install dropbox

--- lib/installs/dropbox.sh
+++ lib/installs/dropbox.sh
@@ -2,19 +2,21 @@
 # http://www.dropbox.com/
 # Install dropbox
 
-determine_bitage
-VER=$RETURN
+#determine_bitage
+#VER=$RETURN
 
-wget http://www.dropbox.com/downloading?os=lnx -O ~/Downloads/dropbox-index.html
+#wget http://www.dropbox.com/downloading?os=lnx -O ~/Downloads/dropbox-index.html
 
-VALLSTABLE=`egrep -o "nautilus-dropbox_[[:digit:]]{0,9}*\.[[:digit:]]{0,9}*\.[[:digit:]]{0,9}*_${VER}\.deb" ~/Downloads/dropbox-index.html`
-echo "${VALLSTABLE}"
-VERNUM=`echo "${VALLSTABLE}"|cut -d "_" -f 2`
+#VALLSTABLE=`egrep -o "nautilus-dropbox_[[:digit:]]{0,9}*\.[[:digit:]]{0,9}*\.[[:digit:]]{0,9}*_${VER}\.deb" ~/Downloads/dropbox-index.html`
+#echo "${VALLSTABLE}"
+#VERNUM=`echo "${VALLSTABLE}"|cut -d "_" -f 2`
 
-rm ~/Downloads/dropbox-index.html
+#rm ~/Downloads/dropbox-index.html
 
-echo "Installing Dropbox v${VERNUM}..."
+#echo "Installing Dropbox v${VERNUM}..."
 
-wget http://www.dropbox.com/download?dl=packages/nautilus-dropbox_${VERNUM}_${VER}.deb -O ~/Downloads/nautilus-dropbox_${VERNUM}_${VER}.deb
-sudo dpkg -i ~/Downloads/nautilus-dropbox_${VERNUM}_${VER}.deb
-rm ~/Downloads/nautilus-dropbox_${VERNUM}_${VER}.deb
+#wget http://www.dropbox.com/download?dl=packages/nautilus-dropbox_${VERNUM}_${VER}.deb -O ~/Downloads/nautilus-dropbox_${VERNUM}_${VER}.deb
+#sudo dpkg -i ~/Downloads/nautilus-dropbox_${VERNUM}_${VER}.deb
+#rm ~/Downloads/nautilus-dropbox_${VERNUM}_${VER}.deb
+
+sudo apt-get install nautilus-dropbox
