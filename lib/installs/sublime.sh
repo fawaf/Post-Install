de8ba055b20c79e4d92049fbb05e68ecc5ff99d7
DungFu <dungfu3@gmail.com>
2012-04-28 16:42:22 -0700

    added sublime installer and other fixes

---
+++ lib/installs/sublime.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## install Sublime Text 2
+
+logfile=/dev/null
+
+echo "Adding Repository..."
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-2 &> $logfile
+sudo apt-get -qq update &> $logfile
+echo -e "What version of Sublime Text 2 would you like to install (1=dev, \033[1m2=beta\033[0m)?"
+read -p "Option: " CONFIRM
+echo ""
+
+if [ "$CONFIRM" = 1 ]; then
+	echo "Downloading and Installing Sublime Text 2 Dev..."
+	sudo apt-get -qqy install sublime-text-2-dev &> $logfile
+elif [ "$CONFIRM" = 2 ]; then
+	echo "Downloading and Installing Sublime Text 2 Beta..."
+	sudo apt-get -qqy install sublime-text-2-beta &> $logfile
+else
+	echo "Downloading and Installing Sublime Text 2 Beta..."
+	sudo apt-get -qqy install sublime-text-2-beta &> $logfile
+fi
